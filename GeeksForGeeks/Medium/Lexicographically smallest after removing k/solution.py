88b18428db5040f220042fb018a1a662260a12c9
Roshan <96565932+roshaniist@users.noreply.github.com>
2026-06-05 17:29:44 +0530

    [AlgoCommit] GeeksForGeeks/Medium/Lexicographically smallest after removing k

---
+++ GeeksForGeeks/Medium/Lexicographically smallest after removing k/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+   def lexicographicallySmallest(self, s, k):
+        # code here 
+        n = len(s)
+        if n&(n-1)==0:
+            k = k//2
+        else:
+            k = k*2
+        if k>=n:
+            return "-1"
+        
+        stack = []
+        for ele in s:
+            while stack and k!=0 and stack[-1]>ele:
+                stack.pop()
+                k-=1
+            stack.append(ele)
+            
+        while k:
+            stack.pop()
+            k-=1
+            
+        return ''.join(stack) if k==0 else "-1"
+        # code here 
+        
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 | # | Problem | Platform | Difficulty | Date Solved | Time |
 |---|---------|----------|------------|-------------|------|
 | 1 | [Missing in Array](https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1?page=1&sortBy=submissions) | GeeksForGeeks | Easy | 05 Jun 2026 | 05:28 pm |
+| 2 | [Lexicographically smallest after removing k](https://www.geeksforgeeks.org/problems/lexicographically-smallest-after-removing-k/1) | GeeksForGeeks | Medium | 05 Jun 2026 | 05:29 pm |
